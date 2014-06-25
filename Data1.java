64888cba6564ef45f425b4bcd4afe60f63d66874
mikankari <mikankari@users.noreply.github.com>
2014-06-25 22:22:38 +0900

    Create Data1.java

---
+++ Data1.java
@@ -0,0 +1,20 @@
+package com.example.conect1;
+
+import com.android.volley.Request;
+import com.android.volley.Response.ErrorListener;
+import com.android.volley.Response.Listener;
+import com.android.volley.toolbox.StringRequest;
+
+public class Data1 extends StringRequest{
+
+	public Data1(int method, String url, Listener<String> listener,
+			ErrorListener errorListener) {
+		super(method, url, listener, errorListener);
+		// TODO 自動生成されたコンストラクター・スタブ
+		
+		
+		
+	}
+
+
+}
